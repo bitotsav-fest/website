6ea24d92dad03bc108128322d18180daf0d676b2
Abhinav Kumar Choudhary <abhinav.kumar.1264@gmail.com>
2025-03-14 18:19:36 +0530

    landing v2

--- src/app/landingv2/page.js
+++ src/app/landingv2/page.js
@@ -4,9 +4,11 @@ import About from "@/components/landing/about"
 import NightEvents from "@/components/landing/night-events"
 import AfterMovie from "@/components/landing/aftermovie"
 
+
 export default function AboutPage() {
   return (
     <>
+      {/* <Model here /> */}
       <About />
       <NightEvents />
       <AfterMovie />
