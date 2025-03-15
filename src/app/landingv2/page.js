d6abb56ffe81632d98fdc888c349de181d227942
Brainitech <gaming.brainiac2020@gmail.com>
2025-03-15 12:52:15 +0530

    Modified IOS Landing Page

--- src/app/landingv2/page.js
+++ src/app/landingv2/page.js
@@ -3,16 +3,18 @@ import React from "react"
 import About from "@/components/landing/about"
 import NightEvents from "@/components/landing/night-events"
 import AfterMovie from "@/components/landing/aftermovie"
+import Model from "@/components/landing/Model"
+import { Nav } from "@/components/landing/NAV"
 
-
-export default function AboutPage() {
+export default function Landing_2() {
   return (
     <>
-    <div className="min-h-screen bg-gradient-to-br from-[#0A0118] via-[#2D1E0F] to-[#1A0B2E] text-[#F6F1E2]">
-      {/* <Model here /> */}
-      <About />
-      <NightEvents />
-      <AfterMovie />
+      <div className="min-h-screen bg-gradient-to-br from-[#0A0118] via-[#2D1E0F] to-[#1A0B2E] text-[#F6F1E2] flex flex-col justify-center items-center">
+        <Nav />
+        <Model />
+        <About />
+        <NightEvents />
+        <AfterMovie />
       </div>
     </>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -11,11 +11,20 @@
 // import Hero from "@/components/landing/Hero"
 // import NightEvents from "@/components/landing/night-events"
 import Landing from "@/components/landing/Landing"
+import Landing_2 from "./landingv2/page"
+import { isIOS } from "@/components/landing/cacheModel"
+
+let LandingComponent
+if (isIOS()) {
+  LandingComponent = Landing_2
+} else {
+  LandingComponent = Landing
+}
 
 export default function Home() {
   return (
     <>
-      <Landing />
+      <LandingComponent />
     </>
   )
 }
