17bc198d0ada2ed38904c4dcd98738aabdd070c0
Mrityunjay Raj <mr.raj.earth@gmail.com>
2025-03-20 05:07:11 +0530

    minor ui fix

--- src/app/leaderboard/page.js
+++ src/app/leaderboard/page.js
@@ -4,7 +4,7 @@ import TeamLeaderboard from './components/TeamLeaderboard';
 export default function LeaderboardPage() {
   return (
     <div className="container mx-auto px-2 py-2">
-      <h1 className="text-3xl font-bold text-center mb-8">Team Competition Leaderboard</h1>
+      <h1 className="text-3xl font-bold text-center mb-10"></h1>
       <TeamLeaderboard />
     </div>
   );
